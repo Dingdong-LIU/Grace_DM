58873d3e8992d2d9df9733db03261586e96fc5ce
TheDoctor-JI <yifanchangle@163.com>
2023-07-22 13:33:24 +0800

    Updat linke

--- utils/dialog_handler.py
+++ utils/dialog_handler.py
@@ -2,7 +2,7 @@ import requests
 import random
 from logging import getLogger
 
-NGROK_LINK = 'https://02a9-143-89-145-170.ngrok-free.app'
+NGROK_LINK = 'https://c0c9-143-89-145-170.ngrok-free.app'
 class DIALOG_HANDLR:
     def __init__(self) -> None:
         self.session_id = random.randint(10000000, 500000000)
